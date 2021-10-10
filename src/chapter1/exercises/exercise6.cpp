b068f69f371583322fbc2537839e87d037e99cf0
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2021-10-10 12:50:06 +0100

    feat: Began exercise 6

--- src/chapter1/exercises/exercise6.cpp
+++ src/chapter1/exercises/exercise6.cpp
@@ -3,3 +3,11 @@
 using std::cin;
 using std::cout;
 
+int main() {
+    cout << "Enter the number (in base 10) to convert to binary (base 2)";
+    std::string input;
+    cin >> input;
+    std::string output;
+    for (int i = 0; i < input.length(); i++){
+    }
+}
